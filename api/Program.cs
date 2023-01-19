c4f5e02da04a9716dbf97d7a03aad051bcdf6906
Ashish <ashish.labade@newvision-software.com>
2023-01-19 17:32:06 +0530

    Step-2

--- api/Program.cs
+++ api/Program.cs
@@ -6,7 +6,9 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=> {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 // HTTP request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); // x is Cors policy builder
 app.MapControllers();
 app.Run();
