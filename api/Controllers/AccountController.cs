19de9041c4a3266553b2d33e72e59cea9baf1e06
Ashish <ashish.labade@newvision-software.com>
2023-01-23 15:48:52 +0530

    Section 37

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,9 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using api.Data;
+using api.DTOs;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 namespace api.Controllers
 {
     public class AccountController : BaseApiController
@@ -14,19 +15,25 @@ namespace api.Controllers
             _context = context;
         }
         [HttpPost("register")] // api/account/register
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await UserExists(registerDto.Username)) return BadRequest("The user: "+registerDto.Username+" already exists");
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
 
             };
             _context.users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.users.AnyAsync(x => x.UserName == username.ToLower());
         }
         
     }
