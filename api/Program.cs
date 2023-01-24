942437c71523a0b72f748fd1a69d8d71302f44e7
Ashish <ashish.labade@newvision-software.com>
2023-01-24 13:02:01 +0530

    Section 42

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Services;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -7,7 +9,9 @@ builder.Services.AddDbContext<DataContext>(opt=> {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
+
 // HTTP request pipeline
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); // x is Cors policy builder
 app.MapControllers();
